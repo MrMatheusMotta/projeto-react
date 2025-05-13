d897e6b2b29b15300af30bd75cda8b237678839c
MrMatheusMotta <sgtmottaskulltech@gmail.com>
2025-05-13 15:05:48 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <header>
+        <h1>Matheus Henrique</h1>
+        <p>3º Sargento da Polícia Militar do Estado do Rio de Janeiro</p>
+      </header>
+      <main>
+        <h2>Sobre mim</h2>
+        <p>Lorem ipsum dolor sit amet <strong>consectetur</strong> adipisicing elit. 
+          Harum dignissimos libero reprehenderit corrupti animi consectetur 
+          nam repellat quae hic incidunt rerum tempora quidem sequi, eum magni 
+          quam provident et. Sint!
+        </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+          Similique itaque nesciunt reprehenderit optio debitis facilis 
+          repudiandae dolore numquam, recusandae voluptate perspiciatis!
+          Iure error quis tempora harum labore similique, non vitae.
+        </p>
+        <h3>Habilidades principais</h3>
+        <ul>
+          <li>Montagem e configuração de Pcs</li>
+          <li>Redes e conectividades</li>
+          <li>Sistemas operacionais ( Windows, Linux, MS-DOS )</li>
+          <li>Didática técnica para cursos e certificações</li>
+        </ul>
+
+        
+      </main>
+      <footer>
+        &copy; Matheus Henrique - Todos os direitos reservados
+
+      </footer>
+    </>
+  )
+}
+
+export default App
